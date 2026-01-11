3c4ea5991e6021dc80d327bda6b4464d653d605b
ganluo960214 <47106040@qq.com>
2026-01-12 00:50:02 +0800

    Update having.rs

--- struct-sql/src/having.rs
+++ struct-sql/src/having.rs
@@ -3,14 +3,14 @@ use crate::condition::Condition;
 use crate::sql_builder::SqlBuilder;
 
 pub trait THaving<'a> {
-    fn having(self, sql_builder: &mut SqlBuilder<'a>);
+    fn having(&'a self, sql_builder: &mut SqlBuilder<'a>);
 }
 pub type Having<'a, COLUMN> = Vec<Condition<'a, COLUMN>>;
 
 impl<'a, COLUMN: Column> THaving<'a> for Having<'a, COLUMN> {
-    fn having(self, sql_builder: &mut SqlBuilder<'a>) {
+    fn having(&'a self, sql_builder: &mut SqlBuilder<'a>) {
         sql_builder.write_sql(" having");
 
-        self.into_iter().for_each(|having| having.condition(sql_builder));
+        self.iter().for_each(|having| having.condition(sql_builder));
     }
 }
