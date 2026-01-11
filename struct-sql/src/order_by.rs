725403d4e0039b272b9bd7a34fa15ccc8ec07534
ganluo960214 <47106040@qq.com>
2026-01-12 00:52:19 +0800

    Update order_by.rs

--- struct-sql/src/order_by.rs
+++ struct-sql/src/order_by.rs
@@ -1,14 +1,14 @@
 use crate::column::Column;
-use crate::sql_builder::SqlBuilder;
+use crate::sql_builder::{SqlArg, SqlBuilder};
 
-pub type OrderBy<COLUMN> = Vec<OrderByItem<COLUMN>>;
+pub type OrderBy<'a, COLUMN> = Vec<OrderByItem<'a, COLUMN>>;
 
-pub trait TOrderBy {
-    fn order_by(&self, sql_builder: &mut SqlBuilder);
+pub trait TOrderBy<'a> {
+    fn order_by(&self, sql_builder: &mut SqlBuilder<'a>);
 }
 
-impl<COLUMN: Column> TOrderBy for OrderBy<COLUMN> {
-    fn order_by(&self, sql_builder: &mut SqlBuilder) {
+impl<'a, COLUMN: Column> TOrderBy<'a> for OrderBy<'a, COLUMN> {
+    fn order_by(&self, sql_builder: &mut SqlBuilder<'a>) {
         sql_builder.write_sql(" order by ");
         for (i, order_by_item) in self.iter().enumerate() {
             if i != 0 {
@@ -20,23 +20,37 @@ impl<COLUMN: Column> TOrderBy for OrderBy<COLUMN> {
 }
 
 #[derive(Clone)]
-pub enum OrderByItem<COLUMN: Column> {
+pub enum OrderByItem<'a, COLUMN: Column> {
     ASC(COLUMN),
     DESC(COLUMN),
+    STMakePoint(COLUMN, SqlArg<'a>, SqlArg<'a>),
 }
 
-impl<COLUMN: Column> OrderByItem<COLUMN> {
-    pub fn order_by_item(&self, sql_builder: &mut SqlBuilder) {
+impl<'a, COLUMN: Column> OrderByItem<'a, COLUMN> {
+    pub fn order_by_item(&self, sql_builder: &mut SqlBuilder<'a>) {
         // let column: &'a dyn Column;
         // let str: &str;
 
-        let (column, r#str) = match self {
-            OrderByItem::ASC(v) => (v, "asc"),
-            OrderByItem::DESC(v) => (v, "desc"),
+        match self {
+            OrderByItem::ASC(v) => {
+                v.column(sql_builder);
+                sql_builder.write_sql(" ");
+                sql_builder.write_sql("asc");
+            }
+            OrderByItem::DESC(v) => {
+                v.column(sql_builder);
+                sql_builder.write_sql(" ");
+                sql_builder.write_sql("desc");
+            }
+            OrderByItem::STMakePoint(column, arg1, arg2) => {
+                column.column(sql_builder);
+                sql_builder.write_sql(" <-> ");
+                sql_builder.write_sql("ST_MakePoint(");
+                sql_builder.push_arg(*arg1);
+                sql_builder.write_sql(", ");
+                sql_builder.push_arg(*arg2);
+                sql_builder.write_sql(")");
+            }
         };
-
-        column.column(sql_builder);
-        sql_builder.write_sql(" ");
-        sql_builder.write_sql(str);
     }
 }
