66334baa452b3a0299b02c701ca3c30afcdf37cf
ganluo960214 <47106040@qq.com>
2026-07-16 20:45:33 +0800

    update

--- struct-sql/src/update_set.rs
+++ struct-sql/src/update_set.rs
@@ -1,6 +1,7 @@
 use crate::column::Column;
 use crate::sql_builder::{SqlArg, SqlBuilder};
 use std::collections::HashMap;
+use std::ops::{Deref, DerefMut};
 
 pub trait TUpdateSet<'a> {
     fn update_set(&self, sql_builder: &mut SqlBuilder<'a>);
@@ -19,7 +20,76 @@ impl<'a> From<SqlArg<'a>> for UpdateValue<'a> {
     }
 }
 
-pub type UpdateSet<'a, COLUMN> = HashMap<COLUMN, UpdateValue<'a>>;
+impl<'a, T: postgres_types::ToSql + Sync + 'a> From<&'a T> for UpdateValue<'a> {
+    fn from(arg: &'a T) -> Self {
+        UpdateValue::Value(arg as SqlArg<'a>)
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct UpdateSet<'a, COLUMN>(pub HashMap<COLUMN, UpdateValue<'a>>);
+
+impl<'a, COLUMN: std::cmp::Eq + std::hash::Hash> UpdateSet<'a, COLUMN> {
+    pub fn new() -> Self {
+        Self(HashMap::new())
+    }
+
+    pub fn insert<V: Into<UpdateValue<'a>>>(&mut self, k: COLUMN, v: V) -> Option<UpdateValue<'a>> {
+        self.0.insert(k, v.into())
+    }
+}
+
+impl<'a, COLUMN> IntoIterator for UpdateSet<'a, COLUMN> {
+    type Item = (COLUMN, UpdateValue<'a>);
+    type IntoIter = std::collections::hash_map::IntoIter<COLUMN, UpdateValue<'a>>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
+
+impl<'a, 'b, COLUMN> IntoIterator for &'b UpdateSet<'a, COLUMN> {
+    type Item = (&'b COLUMN, &'b UpdateValue<'a>);
+    type IntoIter = std::collections::hash_map::Iter<'b, COLUMN, UpdateValue<'a>>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.iter()
+    }
+}
+
+impl<'a, COLUMN> Deref for UpdateSet<'a, COLUMN> {
+    type Target = HashMap<COLUMN, UpdateValue<'a>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<'a, COLUMN> DerefMut for UpdateSet<'a, COLUMN> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl<'a, COLUMN: Column + std::cmp::Eq + std::hash::Hash, V: Into<UpdateValue<'a>>, const N: usize> From<[(COLUMN, V); N]> for UpdateSet<'a, COLUMN> {
+    fn from(arr: [(COLUMN, V); N]) -> Self {
+        let mut map = HashMap::with_capacity(N);
+        for (k, v) in arr {
+            map.insert(k, v.into());
+        }
+        UpdateSet(map)
+    }
+}
+
+impl<'a, COLUMN: Column + std::cmp::Eq + std::hash::Hash, V: Into<UpdateValue<'a>>> FromIterator<(COLUMN, V)> for UpdateSet<'a, COLUMN> {
+    fn from_iter<T: IntoIterator<Item = (COLUMN, V)>>(iter: T) -> Self {
+        let mut map = HashMap::new();
+        for (k, v) in iter {
+            map.insert(k, v.into());
+        }
+        UpdateSet(map)
+    }
+}
 
 impl<'a, COLUMN: Column> TUpdateSet<'a> for UpdateSet<'a, COLUMN> {
     fn update_set(&self, builder: &mut SqlBuilder<'a>) {
